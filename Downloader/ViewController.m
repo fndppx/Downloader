e138991aa508466f04ecc965d8bdc6b33bd4f1eb
lcs <weikeyan@imooc.com>
2020-11-13 11:22:26 +0800

    完善

--- Downloader/ViewController.m
+++ Downloader/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import "SXTDownload.h"
+#import "SXTDownloadConst.h"
 @interface ViewController ()
 
 @end
@@ -17,8 +18,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *downLoadURL = @"http://www.ikebang.com/book/down?timestamp=1605233802&url=5c1075b483ba88a9535e171e&token=76a32c75e668826dab72287e01c812de";
+    
+    SXTDownloadModel *model = [SXTDownloadModel new];
+    model.url = downLoadURL;
+    model.vid = @"1";
+    model.localPath = NSHomeDirectory();
+    model.fileName = @"测试";
+    [[SXTDownloadManager shareManager]startDownloadTask:model];
+    
+//    NSString * const SXTDownloadProgressNotification = @"SXTDownloadProgressNotification";
+//    NSString * const SXTDownloadStateChangeNotification = @"SXTDownloadStateChangeNotification";
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(downloadProgressNotification:) name:SXTDownloadProgressNotification object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(downloadStateChangeNotification:) name:SXTDownloadStateChangeNotification object:nil];
 }
 
+- (void)downloadStateChangeNotification:(NSNotification*)notify{
+    
+}
+
+- (void)downloadProgressNotification:(NSNotification*)notify{
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
